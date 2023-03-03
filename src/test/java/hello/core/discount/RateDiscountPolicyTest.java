35ee101794f8c23b6cf9da9b55ec82c83a60c4d9
hong <tlsrl6427@naver.com>
2023-03-03 23:31:51 +0900

    init

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,43 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    //JUnit5부터 지원
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_o(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다")
+    void vip_x(){
+        //given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
